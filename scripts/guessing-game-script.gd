120100f5ec755a0b0624866ed0a9f1c2176c9bdc
Matheus <matheus7rod@gmail.com>
2025-06-05 20:14:10 -0300

    oi

--- scripts/guessing-game-script.gd
+++ scripts/guessing-game-script.gd
@@ -1,57 +1,44 @@
 extends Node
 
-
-@onready var continues_display: Label = $"CanvasLayer/Continues Container/MarginContainer/GridContainer/Continues display"
-@onready var line_edit: LineEdit = $LineEdit
-@onready var button: Button = $Button
-
+@onready var continues_display: Label = %ContinueDisplay
+@onready var line_edit: LineEdit = %LineEdit
+@onready var button: Button = %Button
 
 var rng = RandomNumberGenerator.new()
-
 var scene_number = rng.randi_range(1, 25)
-
 var half_number = int(scene_number / 2)
-
 var continues = 5
-
-# Handles the numbers and continues
 
 
 func _on_line_edit_text_submitted(new_text: String) -> void:
 	rng.randomize()
-	var player_number = int(line_edit.text)
+	var player_number = int(line_edit.text) 
 	if player_number == scene_number:
-		continues_display.text = 'Sucess! ' # Try again messages
+		continues_display.text = 'Sucess! '
 	elif player_number <= half_number:
 			continues = continues -1
 			continues_display.text = 'Almost there! Continues: ' + str(continues)
+	#Comparacao desnecessária a mais aqui, se o player_number for maior que scene_number então ele sempre vai ser maior ou igual que half_number, que é metade de scene_number.
 	elif player_number > scene_number and player_number >= half_number:
 		continues = continues -1
 		continues_display.text = 'Wow there pal, slow down.'
+	#Puta mensagem enigmática para representar que é maior que half_number e menor que scene_number.
 	elif player_number >= half_number:
 		continues = continues -1
 		continues_display.text = 'A little, too much? Perhaps... Continues: ' + str(continues)
 	if continues <= 0:
-		button_visibility() # Calls the function everytime the player runs out of continues
+		button_visibility()
 		continues_display.text = "The Cheese, chees’ndt. Good luck next time!"
 
 
-
-
-
-
-
-# Restarts the game
 func _on_button_pressed() -> void:
 	get_tree().reload_current_scene()
 	
-	
-# Button visibility
 
 func _ready():
+	continues_display.text = ''
 	button_visibility()
 
-# Hides the button when the player have continues
 
 func button_visibility():
 	if continues > 0:
