40ee2d3315b46ba75990e4eb218d02e55a9bd6a5
Hyuri <3005.iwkr.tnk@gmail.com>
2025-06-06 18:11:38 -0300

    Working UI and game logic

--- scripts/guessing-game-script.gd
+++ scripts/guessing-game-script.gd
@@ -1,9 +1,9 @@
 extends Node
 
+@onready var continues_display: Label = %ContinuesDisplay
+@onready var line_edit: LineEdit = $CanvasLayer/VBoxContainer/LineEdit
+@onready var button: Button = $CanvasLayer/VBoxContainer/Button
 
-@onready var continues_display: Label = $"CanvasLayer/Continues Container/MarginContainer/GridContainer/Continues display"
-@onready var line_edit: LineEdit = $LineEdit
-@onready var button: Button = $Button
 
 
 var rng = RandomNumberGenerator.new()
@@ -25,14 +25,14 @@ func _on_line_edit_text_submitted(new_text: String) -> void:
 	elif player_number <= half_number:
 			continues = continues -1
 			continues_display.text = 'Almost there! Continues: ' + str(continues)
-	elif player_number > scene_number and player_number >= half_number:
+	elif player_number > scene_number:
 		continues = continues -1
 		continues_display.text = 'Wow there pal, slow down.'
 	elif player_number >= half_number:
 		continues = continues -1
-		continues_display.text = 'A little, too much? Perhaps... Continues: ' + str(continues)
+		continues_display.text = 'A little, too much? Continues: ' + str(continues)
 	if continues <= 0:
-		button_visibility() # Calls the function everytime the player runs out of continues
+		button_visibility() 
 		continues_display.text = "The Cheese, chees’ndt. Good luck next time!"
 
 
